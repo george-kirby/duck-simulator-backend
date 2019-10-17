6f889e9a6ad695f02789f2a3fb7d0771951e8d28
george-kirby <georgeskirby@btinternet.com>
2019-10-17 16:01:21 +0100

    added serializers, improved seed data

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,6 +1,8 @@
 class AreasController < ApplicationController
     def index
         areas = Area.all
-        render json: areas
+        render json: areas.to_json(:include => {
+            :ducks => {:only => [:name, :image_url, :id] }
+        }, :except => [:created_at, :updated_at])
     end
 end
